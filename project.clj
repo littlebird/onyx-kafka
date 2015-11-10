26c7af53c8c3e4ec972a05c39c7d9fd107089c07
Ryan Spangler <ryan@getlittlebird.com>
2015-11-09 22:20:37 -0800

    set up for littlebird deploy

--- project.clj
+++ project.clj
@@ -1,16 +1,8 @@
-(defproject org.onyxplatform/onyx-kafka "0.8.0-alpha1"
+(defproject aviary/onyx-kafka "0.8.1"
   :description "Onyx plugin for Kafka"
   :url "https://github.com/MichaelDrogalis/onyx-kafka"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :repositories {"snapshots" {:url "https://clojars.org/repo"
-                              :username :env
-                              :password :env
-                              :sign-releases false}
-                 "releases" {:url "https://clojars.org/repo"
-                             :username :env
-                             :password :env
-                             :sign-releases false}}
   :dependencies [[org.clojure/clojure "1.7.0"]
                  ^{:voom {:repo "git@github.com:onyx-platform/onyx.git" :branch "master"}}
                  [org.onyxplatform/onyx "0.8.0-alpha1"]
@@ -23,4 +15,13 @@
                              [lein-set-version "0.4.1"]
                              [lein-update-dependency "0.1.2"]
                              [lein-pprint "1.1.1"]]}
-             :circle-ci {:jvm-opts ["-Xmx4g"]}})
+             :circle-ci {:jvm-opts ["-Xmx4g"]}}
+  :plugins [[s3-wagon-private "1.1.2"]]
+  :repositories ^:replace
+  [["central" {:url "http://repo1.maven.org/maven2"}]
+   ["clojure" {:url "http://build.clojure.org/releases"}]
+   ["clojure-snapshots" {:url "http://build.clojure.org/snapshots"}]
+   ["clojars" {:url "http://clojars.org/repo/"}]
+   ["private" {:url "s3p://littlebird-maven/releases/"
+               :creds :gpg
+               :sign-releases false}]])
